aca98b954d422c22946ecaaabe23fd29d8a8453a
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-11-15 08:47:34 +0000

    backup

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8080
 export CLASSPATH=/usr/share/tomcat7/lib/*
 javac src/q?.java -d web/WEB-INF/classes
 
+sudo cp other/catalina.properties /etc/tomcat7/catalina.properties
+
 sudo cp -r web /usr/share/tomcat7/webapps/ROOT
 sudo service tomcat7 restart
--- q3.sh
+++ q3.sh
@@ -3,3 +3,4 @@ export CLASSPATH=/usr/share/tomcat7/lib/*:`hbase classpath`
 javac src/q3.java -d web/WEB-INF/classes
 
 sudo cp web/WEB-INF/classes/mypackage/q3.class /usr/share/tomcat7/webapps/ROOT/WEB-INF/classes/mypackage/q3.class
+sudo cp web/WEB-INF/web.xml /usr/share/tomcat7/webapps/ROOT/WEB-INF/web.xml
