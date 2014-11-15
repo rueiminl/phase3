33e47b24861a0c8975370b5ba39e0dbbdf5e741a
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-11-15 17:30:42 +0000

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8080
 
-export CLASSPATH=/usr/share/tomcat7/lib/*
-javac src/q?.java -d web/WEB-INF/classes
-
 sudo cp other/catalina.properties /etc/tomcat7/catalina.properties
 
-sudo cp -r web /usr/share/tomcat7/webapps/ROOT
-sudo service tomcat7 restart
+export CLASSPATH=/usr/share/tomcat7/lib/*:`hbase classpath`
+javac src/q?.java -d web/WEB-INF/classes
+sudo mkdir /usr/share/tomcat7/webapps/ROOT/
+sudo cp -r web/WEB-INF /usr/share/tomcat7/webapps/ROOT
+sudo service tomcat7 start
