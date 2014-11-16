82301c41a2259d9497bdd428c1b0e5ce41fbf4c3
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-11-16 22:57:34 +0000

    init

--- src/q1.java
+++ src/q1.java
@@ -8,9 +8,12 @@ import java.text.*;
 
 // Extend HttpServlet class
 public class q1 extends HttpServlet {
-
+    BigInteger x;
+    SimpleDateFormat ft;
     public void init() throws ServletException {
         // Do required initialization
+    	x = new BigInteger("6876766832351765396496377534476050002970857483815262918450355869850085167053394672634315391224052153");
+    	ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -19,10 +22,8 @@ public class q1 extends HttpServlet {
         response.setContentType("text/plain");
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
-        BigInteger x = new BigInteger("6876766832351765396496377534476050002970857483815262918450355869850085167053394672634315391224052153");
 	BigInteger xy = new BigInteger(request.getParameter("key"));
         BigInteger y = xy.divide(x);
-	SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
         out.println(y + "\nWolken,5534-0848-5100,0299-6830-9164\n" + ft.format(new Date()));
     }
 
