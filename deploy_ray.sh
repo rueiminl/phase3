76f937e94305df8839511a21b43913f8211a1bf0
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-11-19 17:54:24 +0000

    add s3cmd --configure

--- deploy_ray.sh
+++ deploy_ray.sh
@@ -5,11 +5,11 @@ sudo cp other/catalina.properties /etc/tomcat7/catalina.properties
 
 sudo mkdir /usr/share/tomcat7/webapps/ROOT/
 
+# note: need s3cmd --configure if no role
 wget http://softlayer-dal.dl.sourceforge.net/project/s3tools/s3cmd/1.5.0-rc1/s3cmd-1.5.0-rc1.tar.gz
 tar zxvf s3cmd-1.5.0-rc1.tar.gz
 cd s3cmd-1.5.0-rc1
 sudo python setup.py install
-s3cmd --configure
 sudo s3cmd get s3://raylin/q6.txt /usr/share/tomcat7/webapps/ROOT
 cd ..
 
